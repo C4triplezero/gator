7be8b4c59e6d64e52643b6769294a12d5a75df7f
C4triplezero <jcurrie3000@gmail.com>
2026-03-05 05:50:27 -0700

    consolidated commands and added reset command for deleting all entries in the users table

--- commands.go
+++ commands.go
@@ -36,3 +36,35 @@ func handlerRegister(s *state, cmd command) error {
 
 	return nil
 }
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.Args) == 0 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+	name := cmd.Args[0]
+
+	_, err := s.DB.GetUser(context.Background(), name)
+	if err != nil {
+		return fmt.Errorf("user: %s does not exist", name)
+	}
+
+	err = s.Config.SetUser(name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Printf("User: %s has been set\n", name)
+
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	err := s.DB.ResetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't reset table: %w", err)
+	}
+
+	fmt.Println("Users table has been reset")
+
+	return nil
+}
